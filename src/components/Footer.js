0f066ded2cb8cc9fc54aaa353cd05a4df6c1f8f5
Sami Kalammallah <skalexsong@gmail.com>
2020-11-02 22:34:10 +0700

    migrate footer css in js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,36 @@
 import React from "react";
 
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { styled } from "@material-ui/core/styles";
+
 export default function Footer() {
+  const Footer = styled(Box)({
+    position: "fixed",
+    left: 0,
+    bottom: 0,
+    width: "100%",
+    textAlign: "center",
+    padding: "5px",
+    backgroundColor: "#c1beba",
+    "& a": {
+      color: "rgb(253, 69, 69)",
+      textDecoration: "none",
+    },
+  });
+
   return (
-    <footer>
-      <p className="footer__text">
+    <Footer>
+      <Typography variant="body1">
         Made with{" "}
-        <span role="img" aria-label="love">
+        <Box component="span" role="img" aria-labelledby="love">
           ♥️
-        </span>{" "}
+        </Box>{" "}
         by
-        <a href="https://samx23.github.io"> Sami Kalammallah</a> ©{" "}
+        <Link href="https://samx23.github.io"> Sami Kalammallah</Link> ©{" "}
         {new Date().getFullYear()}
-      </p>
-    </footer>
+      </Typography>
+    </Footer>
   );
 }

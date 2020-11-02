2d09b95dc3e46d27dce81c6a29d511e58c58b577
Sami Kalammallah <skalexsong@gmail.com>
2020-11-02 22:38:55 +0700

    migrate home css in js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,37 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Box from "@material-ui/core/Box";
+import { styled } from "@material-ui/core/styles";
+
 const Chat = lazy(() => import("../components/Chat"));
 const Sidebar = lazy(() => import("../components/Sidebar"));
 
 export default function Home() {
+  const AppBody = styled(Box)({
+    display: "flex",
+    backgroundColor: "#ededed",
+    marginTop: " -30px",
+    height: "90vh",
+    width: "90vw",
+    boxShadow: "-1px 4px 20px -6px rgba(0, 0, 0, 0.5)",
+  });
+
   return (
-    <Suspense fallback={<div>Loading Home...</div>}>
-      <div className="app__body">
+    <Suspense fallback={<Box>Loading Home...</Box>}>
+      <AppBody>
         <Router>
           <Sidebar />
           <Switch>
             <Route path="/rooms/:roomId">
-              <Suspense fallback={<div>Loading Chat...</div>}>
+              <Suspense fallback={<Box>Loading Chat...</Box>}>
                 <Chat />
               </Suspense>
             </Route>
             <Route path="/"></Route>
           </Switch>
         </Router>
-      </div>
+      </AppBody>
     </Suspense>
   );
 }

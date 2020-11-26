4cd829341e2a56096b57a00de07450f0bea61567
SamX23 <skalexsong@gmail.com>
2020-11-26 21:29:32 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDH_Gdh0c6z4i6_S1tlsBXlsb3TusoCkP0",
-  authDomain: "search-engine-sam.firebaseapp.com",
-  databaseURL: "https://search-engine-sam.firebaseio.com",
-  projectId: "search-engine-sam",
-  storageBucket: "search-engine-sam.appspot.com",
-  messagingSenderId: "1035026594476",
-  appId: "1:1035026594476:web:2c04a67ad600c68a127d09",
-  measurementId: "G-G2BPEM6N7S",
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURE_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);

0e388d5a12747a8890346eb11896dd656d59bb0a
SamX23 <skalexsong@gmail.com>
2021-01-01 22:12:11 +0700

    register service worker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { theme } from "./styles/GlobalStyles";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { StateProvider } from "./store/StateProvider";
 import reducer, { initialState } from "./store/reducer";
+import * as serviceWorker from "./service-worker";
 
 ReactDOM.render(
   <StateProvider initialState={initialState} reducer={reducer}>

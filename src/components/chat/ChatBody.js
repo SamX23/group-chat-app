3d49e8f1cd3476d718de6ee6b10cb17f12abb946
samx23 <skalexsong@gmail.com>
2021-03-02 23:53:41 +0700

    Add scroll to bottom using ref

--- src/components/chat/ChatBody.js
+++ src/components/chat/ChatBody.js
@@ -1,30 +1,43 @@
 import PropTypes from "prop-types";
+import { useEffect, useRef } from "react";
 import MessageFormatter from "../MessageFormatter";
 
-const ChatBody = ({ messages, showDate, user }) => (
-  <div className="chat__body">
-    <MessageFormatter>
-      {messages.map((message) => (
-        <p
-          key={`${message.name}-${message.timestamp}`}
-          className={`chat__message ${
-            message.uid === user.uid && "chat__sender"
-          }`}
-        >
-          <span
-            className={`chat__name ${
-              message.uid === user.uid && "chat__nameSender"
+const ChatBody = ({ messages, showDate, user }) => {
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <div className="chat__body">
+      <MessageFormatter>
+        {messages.map((message) => (
+          <p
+            key={`${message.name}-${message.timestamp}`}
+            className={`chat__message ${
+              message.uid === user.uid && "chat__sender"
             }`}
           >
-            {message.name}
-          </span>
-          {message.message}
-          <span className="chat__timestamp">{showDate(message)}</span>
-        </p>
-      ))}
-    </MessageFormatter>
-  </div>
-);
+            <span
+              className={`chat__name ${
+                message.uid === user.uid && "chat__nameSender"
+              }`}
+            >
+              {message.name}
+            </span>
+            {message.message}
+            <span className="chat__timestamp">{showDate(message)}</span>
+          </p>
+        ))}
+      </MessageFormatter>
+      <div ref={messagesEndRef} />
+    </div>
+  );
+};
 
 ChatBody.propTypes = {
   messages: PropTypes.instanceOf(Array),

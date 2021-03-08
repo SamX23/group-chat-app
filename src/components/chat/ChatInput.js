91bf475b4498420e740f7c45f054b259565c8e48
samx23 <skalexsong@gmail.com>
2021-03-08 14:16:33 +0700

    refactor emoji picker as independent file

--- src/components/chat/ChatInput.js
+++ src/components/chat/ChatInput.js
@@ -2,11 +2,9 @@ import { useState } from "react";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-import { InsertEmoticon } from "@material-ui/icons";
-import { Popover, IconButton, Input, Button, Box } from "@material-ui/core";
+import { Input, Button, Box } from "@material-ui/core";
 import PropTypes from "prop-types";
-import "emoji-mart/css/emoji-mart.css";
-import { Picker } from "emoji-mart";
+import EmojiPicker from "../../globals/EmojiPicker";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -43,8 +41,10 @@ const useStyles = makeStyles(() =>
 const ChatInput = ({ db, roomId, user }) => {
   const classes = useStyles();
   const [message, setMessage] = useState("");
-  const [showPicker, setShowpicker] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
+
+  const addEmoji = (emoji) => {
+    setMessage(message + emoji.native);
+  };
 
   const sendMessage = (event) => {
     event.preventDefault();
@@ -69,45 +69,9 @@ const ChatInput = ({ db, roomId, user }) => {
     setMessage("");
   };
 
-  const handleClick = (event) => {
-    event.preventDefault();
-    setShowpicker(!showPicker);
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const addEmoji = (emoji) => {
-    setMessage(message + emoji.native);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? "emoticon-popover" : undefined;
-
   return (
     <Box className={classes.root}>
-      <IconButton aria-label="insert emoticon" onClick={handleClick}>
-        <InsertEmoticon />
-      </IconButton>
-
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-      >
-        <Picker showPreview={false} showSkinTones={false} onSelect={addEmoji} />
-      </Popover>
+      <EmojiPicker addEmoji={addEmoji} />
 
       <form className={classes.form}>
         <Input

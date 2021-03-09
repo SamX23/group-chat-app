c377f19c82125eddbcfc675cbfb3f2da3a4cb888
samx23 <skalexsong@gmail.com>
2021-03-09 21:15:02 +0700

    fix loading component width

--- src/components/animations/Loading.js
+++ src/components/animations/Loading.js
@@ -1,14 +1,20 @@
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { styled } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 
 function Loading({ title }) {
+  const LoadingArea = styled(Grid)({
+    margin: "auto",
+    textAlign: "center",
+  });
+
   return (
-    <Grid>
+    <LoadingArea>
       <CircularProgress />
       <Box>{title || "Loading..."}</Box>
-    </Grid>
+    </LoadingArea>
   );
 }
 

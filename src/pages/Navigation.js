415219a559e30a9ccb3636cb7d88dc0bc303c32d
samx23 <skalexsong@gmail.com>
2021-03-11 14:47:35 +0700

    add app container

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 import { styled } from "@material-ui/core/styles";
 import { auth } from "../firebase";
 import { actionTypes } from "../store/reducer";
@@ -22,20 +23,21 @@ export default function Navigation() {
     return () => listener();
   }, [dispatch]);
 
-  const AppBackground = styled(Grid)({
+  const AppBackground = styled(Box)({
     backgroundImage:
       "linear-gradient(to right top, #4f4f4f, #65656d, #7a7c8c, #8f94ad, #a2adcf, #9fbde2, #9acdf2, #93deff, #74e7ff, #52f0fe, #37f8f2, #3dffdd)",
+    height: "100vh",
+    width: "100vw",
+    overflow: "auto",
+  });
+
+  const AppContainer = styled(Container)({
+    padding: "3em",
   });
 
   return (
-    <AppBackground
-      container
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: "100vh" }}
-    >
-      {!user ? <Login /> : <App />}
+    <AppBackground>
+      <AppContainer>{!user ? <Login /> : <App />}</AppContainer>
     </AppBackground>
   );
 }
